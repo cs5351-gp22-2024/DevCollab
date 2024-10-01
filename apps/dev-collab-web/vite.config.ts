ffe31def5e07fd2feb3ac9f35dddc3b3ec5c73e4
Isaac <isaac.lai@onepagehk.com>
2024-10-01 12:50:11 +0800

    add tailwind

--- apps/dev-collab-web/vite.config.ts
+++ apps/dev-collab-web/vite.config.ts
@@ -3,20 +3,32 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import sass from 'sass'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/_variables.scss";`,
+        additionalData: `
+          @import "@/assets/_variables.scss";
+          @import "@/assets/_mixins.scss";
+          @import "@/assets/_responsive.scss";
+        `,
         implementation: sass,
         sassOptions: {
           quietDeps: true
         },
-        silenceDeprecations: ['legacy-js-api']
+        silenceDeprecations: ['legacy-js-api'],
       }
-    }
+    },
+    postcss: {
+      plugins: [
+        tailwindcss('./tailwind.config.ts'),
+        autoprefixer,
+      ],
+    },
   },
   resolve: {
     alias: {
