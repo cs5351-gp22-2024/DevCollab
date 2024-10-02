e00ae7cacd6af2bf0bdfe5e437963edbae5b9473
Ronnnnnn <ronnnnn9513cha@gmail.com>
2024-10-02 15:00:32 +0800

    LOGIN PAGE
    + Front end Login Page (Email, Password, Verification)
    + Update Landing Page Components

---
+++ apps/dev-collab-web/src/components/cover_page_components/ConnectBtn.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  methods: {
+    goLogin() {
+      this.$router.push({ name: 'login' })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.red-text-1 {
+  color: $red-1;
+}
+.large-text {
+  font-size: 6rem;
+}
+.bg-red-1 {
+  background-color: $red-1;
+}
+.color-white {
+  color: $vt-c-white;
+}
+</style>
+<template>
+  <div>
+    <button @click="goLogin" class="large-text my-auto bg-red-1 color-white p-6 rounded-5">
+      CONNECT
+    </button>
+
+    <div class="fs-1 red-text-1 fw-bolder mt-6">EVERY</div>
+    <div class="fs-3 red-text-1 fw-bolder">
+      <span> #Team </span>
+      <span> #Task </span>
+      <span> #Project </span>
+    </div>
+  </div>
+</template>
