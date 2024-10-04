c08a514c47c2f58db470125f8eb6e14cc3cd85b5
SIN Yuk Yuen <yysin8-c@my.cityu.edu.hk>
2024-10-05 00:24:07 +0800

    Update morgan

--- apps/dev-collab-api/src/index.ts
+++ apps/dev-collab-api/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan"; 
 import { HomeMessageModel } from "shared/models/home";
 
 const app = express();
 const port = 3000;
+
+// Use morgan to log HTTP requests
+app.use(morgan('dev')); 
 
 app.get("/api/home/messages", (req, res) => {
   res.send(["Hello World!", "Test from Home"] satisfies HomeMessageModel);
