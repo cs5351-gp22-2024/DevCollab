4d707a129aa693c924fe94d7491fbfac678b1e2d
Isaac <skl05310@gmail.com>
2024-10-06 16:21:57 +0800

    minor fix

--- apps/dev-collab-api/src/db/db-datasrc.ts
+++ apps/dev-collab-api/src/db/db-datasrc.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
   host: "127.0.0.1",
   port: 3306,
   username: "root",
-  password: "password",
+  password: "P@ssw0rd",
   database: "DevCollab",
   synchronize: false, // stop creating table automatically
   logging: true,
