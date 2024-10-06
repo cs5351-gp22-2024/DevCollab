b6c09f2991b9358e7c82b2010fe8e44a4e10dc2c
Isaac <skl05310@gmail.com>
2024-10-06 15:18:43 +0800

    update Comments with function

--- apps/dev-collab-web/src/components/Comment/CommentItem.vue
+++ apps/dev-collab-web/src/components/Comment/CommentItem.vue
@@ -1,34 +1,26 @@
 <template>
-  <div
-    :class="['bg-white rounded-lg shadow p-4 mb-4', { 'ml-8 border-l-4 border-blue-500': isReply }]"
-  >
+  <div :class="['bg-white rounded-lg shadow p-4 mb-4', { 'border-l-4 border-blue-500': isReply }]">
     <div class="flex items-center mb-2">
       <img :src="avatarUrl" :alt="comment.author" class="w-8 h-8 rounded-full mr-3" />
-      <div>
+      <div class="w-full flex justify-between">
         <span class="font-bold text-gray-800">{{ comment.author }} - {{ comment.role }}</span>
         <span class="text-sm text-gray-500 ml-2">{{ comment.date }}</span>
       </div>
     </div>
-    <p class="text-gray-700 mb-3">{{ comment.content }}</p>
-    <button
-      class="bg-blue-500 text-white px-3 py-1 rounded-md text-sm hover:bg-blue-600 transition duration-300"
-    >
-      Reply
-    </button>
-
-    <div v-if="comment.replies && comment.replies.length > 0" class="mt-4">
-      <CommentItem
-        v-for="reply in comment.replies"
-        :key="reply.id"
-        :comment="reply"
-        :isReply="true"
-      />
-    </div>
+    <p class="text-gray-700 comments" v-html="comment.content"></p>
   </div>
 </template>
 
+<style lang="scss" scoped>
+.comments {
+  :deep(span) {
+    color: $primary-color;
+  }
+}
+</style>
+
 <script lang="ts">
-import { defineComponent, computed, PropType } from 'vue'
+import { defineComponent, computed, type PropType } from 'vue'
 
 interface Comment {
   id: number
@@ -36,7 +28,6 @@ interface Comment {
   role: string
   date: string
   content: string
-  replies: Comment[]
 }
 
 export default defineComponent({
--- apps/dev-collab-web/src/pages/component/ComponentPage.vue
+++ apps/dev-collab-web/src/pages/component/ComponentPage.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container mx-auto p-4">
     <h1 class="text-4xl font-bold mb-6">Component Demo</h1>
-    <CommentSection :comments="dummyCommentList" />
+    <section class="w-full flex justify-start">
+      <CommentSection class="max-w-2xl" :comments="dummyCommentList" />
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import CommentSection from '@/components/common/CommentSection.vue'
+import CommentSection from '@/components/Comment/CommentSection.vue'
 
 export default defineComponent({
   components: {
@@ -21,25 +23,21 @@ export default defineComponent({
         role: 'Developer',
         date: '9 Sep 17:25',
         content:
-          'I just finished working on this part, please check it out! I just finished working on this part, please check it out! I just finished working on this part, please check it out! I just finished working on this part, please check it out!',
-        replies: []
+          '<span>@Peter Pan</span>\nI just finished working on this part, please check it out! I just finished working on this part, please check it out! I just finished working on this part, please check it out! I just finished working on this part, please check it out!'
       },
       {
         id: 2,
         author: 'Peter Pan',
         role: 'Project Manager',
         date: '9 Sep 17:30',
-        content: 'Thanks.\n@Miss Tester Please take a look first.',
-        replies: [
-          {
-            id: 3,
-            author: 'Alex Lee',
-            role: 'Tester',
-            date: '9 Sep 17:35',
-            content: '@Peter Pan - Project Manager No problem.',
-            replies: []
-          }
-        ]
+        content: 'Thanks.\n<span>@Alex Lee</span> Please take a look first.'
+      },
+      {
+        id: 3,
+        author: 'Alex Lee',
+        role: 'Tester',
+        date: '9 Sep 17:35',
+        content: '<span>@Peter Pan</span> No problem.'
       }
     ])
 
