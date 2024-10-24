4af47e70e835d0406373575e9c7cee0f70c3c2d2
FelixK <43415565+Felix-Ku@users.noreply.github.com>
2024-10-24 22:33:12 +0800

    Revert "Relocate sprint button (#194)"
    This reverts commit e039eb0b2a0f3de1a3b73afe70bdf8bfdde29e4d.

--- apps/dev-collab-web/src/pages/project-sprint/ProjectSprintPage.vue
+++ apps/dev-collab-web/src/pages/project-sprint/ProjectSprintPage.vue
@@ -54,28 +54,16 @@
                     showNow: false,
                     showPreview: true
                   }"
-                >
-                  <template #action-buttons>
-                    <v-btn
-                      color="primary"
-                      size="small"
-                      @click="addSprint()"
-                      v-if="mode === 'create'"
-                    >
-                      Create
-                    </v-btn>
-                    <v-btn
-                      color="primary"
-                      size="small"
-                      @click="updateSprint()"
-                      v-else-if="mode === 'edit'"
-                    >
-                      Save
-                    </v-btn>
-                  </template>
-                </vue-date-picker>
+                ></vue-date-picker>
               </v-list-item>
             </v-list>
+
+            <div class="flex justify-end">
+              <v-btn color="primary" @click="addSprint()" v-if="mode === 'create'">Create</v-btn>
+              <v-btn color="primary" @click="updateSprint()" v-else-if="mode === 'edit'">
+                Save
+              </v-btn>
+            </div>
           </v-col>
         </v-row>
       </v-container>
