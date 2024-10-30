e100080958b5d6cb35824841aadd6e586ede09c1
Ronnnnnn <ronnnnn9513cha@gmail.com>
2024-10-30 23:32:31 +0800

    added get userGroupList

--- apps/dev-collab-api/src/auth/context-user.ts
+++ apps/dev-collab-api/src/auth/context-user.ts
@@ -1,9 +1,11 @@
 import { injectable } from "inversify";
 import { UserAccount } from "../function/user-account";
 import { User } from "../entities/user";
+import { UserGroup } from "../function/user-group";
 
 export interface IContextUser {
   getUserId(token: string | null): Promise<number | null>;
+  getUserGroupList(token: string | null): Promise<number[] | null>;
 }
 
 @injectable()
@@ -16,6 +18,21 @@ export class ContextUser implements IContextUser {
     const user = await UserAccount.checkJWT(token);
     if (user.result == "SUCCESS") {
       return user.user!.userId;
+    } else {
+      return null;
+    }
+  }
+  async getUserGroupList(token: string | null): Promise<number[] | null> {
+    if (token == null) return null;
+    const user = await UserAccount.checkJWT(token);
+    if (user.result == "SUCCESS") {
+      let groupList_result = await UserGroup.getGroupList(user.user!.userId);
+      let groupList: number[] = [];
+      for (let member of groupList_result) {
+        if (member.group != null)
+          groupList[groupList.length] = member.group.group_id;
+      }
+      return groupList;
     } else {
       return null;
     }
